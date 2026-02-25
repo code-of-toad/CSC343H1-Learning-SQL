b66c210a5110d419826ba47df67355ed55b2f380
toad <codeoftoad@gmail.com>
2026-02-24 19:09:04 -0500

    Create back-up of assignment 2 starter code

--- assignment_2/starter_code/part1/q1.sql
+++ assignment_2/starter_code/part1/q1.sql
@@ -14,12 +14,26 @@ CREATE TABLE q1(
 
 -- You may find it convenient to do this for each of the views
 -- that define your intermediate steps. (But give them better names!)
-DROP VIEW IF EXISTS IntermediateStep CASCADE;
+DROP VIEW IF EXISTS Bought CASCADE;
+DROP VIEW IF EXISTS CustomersWithUnratedProduct CASCADE;
 
--- Define views for your intermediate steps here:
-CREATE VIEW IntermediateStep AS ... ;
+-- (CID, IID) pairs: items each customer has purchased
+CREATE VIEW Bought AS
+SELECT DISTINCT p.CID, li.IID
+FROM Purchase p
+JOIN LineItem li ON p.PID = li.PID;
 
-
+-- Customers who bought at least one item they did not review
+CREATE VIEW CustomersWithUnratedProduct AS
+SELECT DISTINCT b.CID
+FROM Bought b
+WHERE NOT EXISTS (
+    SELECT 1 FROM Review r
+    WHERE r.CID = b.CID AND r.IID = b.IID
+);
 
 -- Your query that answers the question goes below the "insert into" line:
 INSERT INTO q1
+SELECT c.CID, c.first_name, c.last_name, c.email
+FROM Customer c
+WHERE c.CID IN (SELECT CID FROM CustomersWithUnratedProduct);
